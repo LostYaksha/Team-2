632e36380f6da3160f237c53c5b0f3e2354f68e1
Rafe <117992417+LostYaksha@users.noreply.github.com>
2025-03-26 02:43:28 +0000

    FAQ
    Added FAQ functions

---
+++ script.js
@@ -0,0 +1,10 @@
+function toggleDetails(button) {
+    const moreDetails = button.nextElementSibling;
+    if (moreDetails.style.display === "none" || moreDetails.style.display === "") {
+        moreDetails.style.display = "block";
+        button.textContent = "Read Less";
+    } else {
+        moreDetails.style.display = "none";
+        button.textContent = "Read More";
+    }
+}
